25a34a20bd5808ec3b3943a41370d0f596281fbb
jwf67 <jwf67@case.edu>
2017-06-25 10:31:34 -0400

    Added base stats file and working on cp calculator

--- pogo_utility.py
+++ pogo_utility.py
@@ -8,40 +8,86 @@ import csv
 #We want the file and desired 
 
 def startPogoUtility():
-	for arg in sys.argv:
-		if arg = "help":
-			helpMenu();
-	if len(sys.argv) != 2:
-		print "Please give 1 argument when running this script with the name of the utility."
+	args = sys.argv
+	for arg in args:
+		if arg == "help":
+			helpMenu()
+			quit()
+	if len(args) != 3:
+		print "Please give 2 argument when running this script with the name of the utility."
+	else:
+		navigateOptions(args[1:])
 
 def helpMenu():
 	print "Here are the following functions available to you (Argument -> Function): "
-	print "ivcalc -> Pogo IV Calculator"
+	print "cpcalc <pokemon name> (ex. cpcalc vaporeon) -> Pogo CP Calculator"
+	print "Restart to use properly. Bye."
 	
+def navigateOptions(args):
+	if args[0] == "cpcalc":
+		cpCalcConfig(args)
+	else:
+		print "Not found. Bye."
+
+def cpCalcConfig(args):
+	ivs = getIVs()
+	base_stats = getBaseStats(args[1])
+	print calculateCP(ivs, base_stats)
+	
+
 def ivCalc():
+	return null
 
 #Given the name of the pokemon, return its base stats from the file
 def getBaseStats(pokemon_name):
-	with open('pokemon_base_stats', 'rb') as f:
+	with open('pokemon_base_stats.csv', 'rb') as f:
 		file = csv.reader(f)
 		for row in file:
-			if(row[0].lower == pokemon_name.lower()):
+			#print row[0].lower()
+			if(row[0].lower() == pokemon_name.lower()):
+				row[1:] = [int(i) for i in row[1:]]
+				print row
+				row[1:] = convertIntsToFloats(row[1:])
 				return row[1:]
+				#for python3, return list(map(int ,row[1:])
 	return "not found";
 
 def getIVs():
 	ivs = [-1, -1, -1]
 	counter = 0
 	hasIvs = [False, False, False]
-	while !hasIvs[0]:
+	while not hasIvs[0]:
 		ivs[0] = input("Please enter your stamina iv: ")
-	while !hasIvs[1]:
+		if isinstance(ivs[0], int) and validIV(ivs[0]):
+			hasIvs[0] = True
+	while not hasIvs[1]:
 		ivs[1] = input("Please enter your attack iv: ")
-	while !hasIvs[2]:
+		if isinstance(ivs[1], int) and validIV(ivs[1]):
+			hasIvs[1] = True
+	while not hasIvs[2]:
 		ivs[2] = input("Please enter your defense iv: ")
+		if isinstance(ivs[2], int) and validIV(ivs[2]):
+			hasIvs[2] = True
+	ivs = convertIntsToFloats(ivs)
+	return ivs
+
+def validIV(iv):
+	if iv >= 0 and iv <= 15:
+		return True
+	return False
 	
 def calculateIvs(base_stats):
+	return null
 
 #Given the base stats of the pokemon and its ivs, calculate its cp
 def calculateCP(ivs, base_stats):
-	CP = ((Base Atk + IV)x(Base Def + IV)^0.5 x (Base Stam + IV)^0.5) / 10
+	CP = ((ivs[1] + base_stats[1])*(ivs[2] + base_stats[2])^0.5 * (ivs[0] + base_stats[0])^0.5)/ 10
+	return CP
+	
+def convertIntsToFloats(arr):
+	for a in arr:
+		float(a)
+	return arr
+
+#Run the functions
+startPogoUtility()
